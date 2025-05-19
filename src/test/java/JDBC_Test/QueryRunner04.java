2fd444b92d2538c93768af9ef484d931f7c65b9c
yigitcam <yigitcam86@gmail.com>
2025-05-19 22:20:13 +0300

    Query 04,05,06 added

---
+++ src/test/java/JDBC_Test/QueryRunner04.java
@@ -0,0 +1,18 @@
+package JDBC_Test;
+
+import org.testng.annotations.Test;
+import utilities.DBUtility;
+
+import java.util.List;
+
+public class QueryRunner04 extends DBUtility {
+    @Test
+    public void query01() {
+        List<List<String>> data = getData("select avg(s.salary) as averageSalaryMan from employees.employees e\n" +
+                "join employees.salaries s on e.emp_no=s.emp_no\n" +
+                "where e.gender='M';");
+        for (List<String> row : data) {
+            System.out.println(row);
+        }
+    }
+}
---
+++ src/test/java/JDBC_Test/QueryRunner06.java
@@ -0,0 +1,21 @@
+package JDBC_Test;
+
+import org.testng.annotations.Test;
+import utilities.DBUtility;
+
+import java.util.List;
+
+public class QueryRunner06 extends DBUtility {
+    @Test
+    public void query01() {
+        List<List<String>> data = getData("select e.emp_no, e.first_name, e.last_name, d.dept_name, s.salary\n" +
+                "from employees.employees e\n" +
+                "join employees.dept_emp de on e.emp_no=de.emp_no\n" +
+                "join employees.departments d on de.dept_no=d.dept_no\n" +
+                "join employees.salaries s on e.emp_no=s.emp_no\n" +
+                "where d.dept_no='d007' and s.salary>70000;");
+        for (List<String> row : data) {
+            System.out.println(row);
+        }
+    }
+}
