a70170897d16e86ace47d44b2a9bdfde6a44f1f7
TugbaKilic <tugbakilic3302@gmail.com>
2025-05-19 22:40:44 +0300

    QueryRunner 7 ,8, 9 done.

---
+++ src/test/java/JDBC_Test/QueryRunner09.java
@@ -0,0 +1,19 @@
+package JDBC_Test;
+
+import org.testng.annotations.Test;
+import utilities.DBUtility;
+
+import java.util.List;
+
+public class QueryRunner09 extends DBUtility {
+    @Test
+    public void query09() {
+        List<List<String>> data = getData("select d.dept_name, avg(s.salary) as averageSalary from employees.salaries s\n" +
+                "join employees.dept_emp de on s.emp_no=de.emp_no\n" +
+                "join employees.departments d on de.dept_no=d.dept_no\n" +
+                "group by d.dept_name;");
+        for (List<String> row : data) {
+            System.out.println(row);
+        }
+    }
+}
