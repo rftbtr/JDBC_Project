f2bbc10025a73a9478b2f4137e18f616e032c5d6
rftbtr <rifatbatir@gmail.com>
2025-05-19 22:40:28 +0300

    Query 01-02-03 fix

--- src/test/java/JDBC_Test/QueryRunner03.java
+++ src/test/java/JDBC_Test/QueryRunner03.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class QueryRunner03 extends DBUtility {
     @Test
-    public void query01() {
+    public void query03() {
         List<List<String>> data = getData("select avg(salary) as averageSalary from employees.salaries;");
         for (List<String> row : data) {
             System.out.println(row);
